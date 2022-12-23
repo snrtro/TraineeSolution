608d98faff91d66a1723384abff958711bb4b5d0
snrtro <107309506+snrtro@users.noreply.github.com>
2022-12-23 09:54:18 +0500

    Initial commit

---
+++ Trainee/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TinkoffTrainee
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var number = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(string.Join(" ", Result(number)));
+        }
+
+        static float Result(int number)
+        {
+            
+        }
+    }
+}
